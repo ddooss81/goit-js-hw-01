f85d242125e7b2f11c2c7557b01a65183d9ea666
ddooss81 <144255208+ddooss81@users.noreply.github.com>
2023-12-05 18:20:33 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function getElementWidth(content, padding, border) {
     return (Number.parseFloat(content) * 100 + Number.parseFloat(padding) * 100 * 2 + Number.parseFloat(border) * 100 * 2) / 100;
 }
